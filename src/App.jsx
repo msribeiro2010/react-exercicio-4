35376c7fa2d4ecd2e29880946d849597ea01e28a
msribeiro2010 <msribeiro2010@icloud.com>
2024-08-28 09:50:22 -0300

    melhorias

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ function App() {
   
 const [password, setPassword] = useState('');
 const [copyText, setCopyText] = useState('Copiar');
+const [passwordSize, setPasswordSize] = useState(12); 
 
  
 function generatePassword() {
      const chars = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-     const length = 12;
      let newPassword = '';
-     for (let i = 0; i < length; i++) {
+     for (let i = 0; i < passwordSize; i++) {
        const position = Math.floor(Math.random() * chars.length);
        newPassword += chars[position]
-
+ 
      }
      setPassword(newPassword); 
   }
@@ -27,7 +27,15 @@ function generatePassword() {
   return (
     <div className="app">
       <h1 >Gerador de Senhas</h1>
-      <button onClick={generatePassword}>Gerar Senha</button>
+      <div><label htmlFor="passwordSize">Tamanho:</label>
+      <input 
+      type="number"  
+      id="passwordSize"  
+      min={1}
+      value={passwordSize}
+      onChange={e => setPasswordSize(e.target.value) }  /> 
+      </div>
+      <button onClick={generatePassword}>Gerar Senha de {passwordSize} caracteres!</button>
       <button onClick={copyPassword}>{copyText}</button>
       <div>{password}</div>
     </div>
